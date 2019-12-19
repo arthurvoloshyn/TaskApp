af9918426ce09501ce04c3543e24b2ccf5d04a84
Artur <grandfaraon93@gmail.com>
2019-12-19 23:10:10 +0200

    Adding testing

---
+++ src/tests/Title.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Title from '../components/title/title';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Title />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
