1f04f4e9bfe4d6400de46d624d40d839028a193b
Artur <grandfaraon93@gmail.com>
2020-01-27 18:08:49 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TaskApp/precache-manifest.9b8d33109b7aefb3502bd5ea3db66abd.js"
+  "/TaskList/precache-manifest.1964c889afe6020dff1fa99449f2c717.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/TaskApp/index.html", {
+workbox.routing.registerNavigationRoute("/TaskList/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
