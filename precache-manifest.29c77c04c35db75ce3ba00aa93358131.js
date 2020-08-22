83d2a830c879af1a5e6bc0bdaec8c25155e13549
ArturW1998 <grandfaraon93@gmail.com>
2020-08-22 20:32:09 +0300

    Updates

---
+++ precache-manifest.29c77c04c35db75ce3ba00aa93358131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baae08b55a44d39fbd674e746b975983",
+    "url": "/TaskApp/index.html"
+  },
+  {
+    "revision": "d822507a3e5093dd9226",
+    "url": "/TaskApp/static/css/main.4da8bbab.chunk.css"
+  },
+  {
+    "revision": "6d0eac49ffa84450716f",
+    "url": "/TaskApp/static/js/2.9b35b938.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TaskApp/static/js/2.9b35b938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d822507a3e5093dd9226",
+    "url": "/TaskApp/static/js/main.9c10d4a7.chunk.js"
+  },
+  {
+    "revision": "382c39984af16bc30b19",
+    "url": "/TaskApp/static/js/runtime-main.53c00261.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TaskApp/precache-manifest.572105f2d8bb6cbbc5902ae280decce0.js"
+  "/TaskApp/precache-manifest.29c77c04c35db75ce3ba00aa93358131.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/TaskApp/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TaskApp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
