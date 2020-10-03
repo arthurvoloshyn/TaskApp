3d96a6ab07e1bba612758aa59e63741d424b80e7
ArturW1998 <grandfaraon93@gmail.com>
2020-10-03 16:45:39 +0300

    Removed the filter from local storage.

--- src/reducers/filters.ts
+++ src/reducers/filters.ts
@@ -1,15 +1,11 @@
-import { load } from 'redux-localstorage-simple';
-
 import { ACTION_TYPES } from '../constants/constants';
 
 import { FilterActionType } from '../types/actions';
 import { Filter } from '../types/store';
 
-const savedFilters: any = load({ namespace: 'task-app' });
+const BASE_FILTER: Filter = 'all';
 
-const initialState: Filter = savedFilters && savedFilters.filters ? savedFilters.filters : 'all';
-
-const filter = (state = initialState, { type, payload }: FilterActionType): Filter => {
+const filter = (state = BASE_FILTER, { type, payload }: FilterActionType): Filter => {
   switch (type) {
     case ACTION_TYPES.CHANGE_FILTER:
       return payload.activeFilter;
